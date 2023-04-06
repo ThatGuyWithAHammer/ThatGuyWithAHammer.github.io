0925e4130ea38842929e0f3122556ee406b2d0db
Enrico Carnevale <shuzolost@gmail.com>
2023-04-06 20:20:18 +0100

    Added back to top button (still not functional)

---
+++ script.js
@@ -0,0 +1,10 @@
+// scroll to top functionality
+const scrollUp = document.querySelector("#scrollUp");
+
+scrollUp.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: "smooth",
+  });
+});
